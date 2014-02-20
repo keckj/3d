812c45733192098bd6919c5f0b72e949695f7efa
Jocelyn MEYRON <Jocelyn.Meyron@ensimag.grenoble-inp.fr>
2014-02-20 11:09:09 +0100

    Add main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,23 @@
-int main (void) {
-    return 0;
+#include <qapplication.h>
+#include "viewer.h"
+#include "objLoader/ObjLoader.h"
+
+int main(int argc, char** argv)
+{
+    // Read command lines arguments.
+    QApplication application(argc,argv);
+
+    // Instantiate the viewer.
+    Viewer viewer;
+
+    // build your scene here
+    viewer.addRenderable(new ObjLoader("src/objLoader/cube.obj"));
+
+    viewer.setWindowTitle("viewer");
+    // Make the viewer window visible on screen.
+    viewer.show();
+
+    // Run main loop.
+    return application.exec();
 }
 
