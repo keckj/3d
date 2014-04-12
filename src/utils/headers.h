6d9123375f56d1dd7b0b5d5316651a62051ab84a
Jean-Baptiste Keck <Jean-Bapiste.Keck@ensimag.grenoble-inp.fr>
2014-04-12 15:43:26 +0200

    headers

---
+++ src/utils/headers.h
@@ -0,0 +1,21 @@
+#ifndef _CUSTOM_HEADERS_H
+#define _CUSTOM_HEADERS_H
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
+
+#include <QWidget>
+#include <QImage>
+#include <QApplication>
+
+#include <QGLViewer/qglviewer.h>
+#include <QGLViewer/vec.h>
+#include <QGLViewer/camera.h>
+#include <QGLViewer/quaternion.h>
+
+#endif /* end of include guard: HEADERS_H */
+
