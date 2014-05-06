99e2af7e6aa50a75cf10497a67329e711560586e
Poulpy <keckj@telesun.imag.fr>
2014-05-06 09:45:09 +0200

    father

---
+++ src/renderable/father.h
@@ -0,0 +1,12 @@
+
+#ifndef FATHER_97WQO4Y8
+#define FATHER_97WQO4Y8
+
+class Father : public RenderTree {
+        public:
+                Father() {};
+                void drawDownwards(const float *currentTransformationMatrix = consts::identity4) {};
+};
+
+#endif /* end of include guard: FATHER_97WQO4Y8 */
+
